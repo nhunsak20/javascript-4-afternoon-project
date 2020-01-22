71f8aeb797b925340685c60b167a544ba1d990be
Nick <nhunsaker20@gmail.com>
2020-01-22 15:14:16 -0700

    Done!!

--- constructors.js
+++ constructors.js
@@ -16,6 +16,11 @@
 
 // Code here
 
+function CarFactory(make, model) {
+  this.make = make
+  this.model = model
+}
+
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
@@ -34,6 +39,9 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+
+  var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
+
   
   ////////// PROBLEM 3 //////////
   
@@ -58,6 +66,16 @@ function Employee(name, email, hireDate) {
   
   // Code here
   
+  function Car(make, model, year) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.move = 0
+    this.moveCar = function() {
+      return this.move += 10
+    }
+  }
+  
   ////////// PROBLEM 4 //////////
   
   /*
@@ -76,6 +94,11 @@ function Employee(name, email, hireDate) {
   }
   
   // Code here
+
+  Movie.prototype.changeRating = function(num) {
+    this.rating += num
+    return this.rating
+  }
   
   ////////// PROBLEM 5 //////////
   
@@ -84,6 +107,16 @@ function Employee(name, email, hireDate) {
   // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
   // Code here
+  function User(name, age, email, savedPosts) {
+    this.name = name
+    this.age = age
+    this.email = email
+    this.savedPosts = savedPosts
+  }
+
+  User.prototype.addSavedPost = function (id, title, rating) {
+    this.savedPosts.push({id, title, rating})
+  }
   
   ////////// PROBLEM 6 //////////
   
@@ -91,6 +124,10 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
   
   // Code here
+
+  User.prototype.removeSavedPost = function(id) {
+    this.savedPosts = this.savedPosts.filter(element => element.id !== id)
+  }
   
   ////////// PROBLEM 7 //////////
   
@@ -98,4 +135,13 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
+
+  User.prototype.changePostRating = function(id, rating) {
+    this.savedPosts = this.savedPosts.filter(element => {
+      if (element.id === id) {
+        element.rating = rating
+      }
+      return element
+    })
+  }
   
--- context.js
+++ context.js
@@ -21,7 +21,13 @@
 
 //Code Here
 
-
+var user = {
+  username: 'nhunsak20',
+  email: 'nhunsaker20@gmail.com',
+  getUsername: function() {
+    return this.username
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -43,6 +49,7 @@ class Animal {
 
 //Code Here
 
+const animal1 = new Animal
 
 
 ////////// PROBLEM 3 //////////
@@ -63,7 +70,7 @@ let who = {
 
 //Code Here
 
-
+var whoSaysHi = sayHi.bind(who)
 
 ////////// PROBLEM 4 //////////
 
@@ -76,7 +83,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = this
 
 let product = {
   name: 'snake plant',
@@ -87,7 +94,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +105,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -116,4 +123,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
